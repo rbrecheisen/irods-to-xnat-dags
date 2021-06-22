a97e5f0bf4531dd63eb84eaaea661f7ba834b536
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2021-06-22 13:18:02 +0200

    First commit

---
+++ dags/dag_rockets.py
@@ -0,0 +1,16 @@
+import airflow
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+
+bash_cmd = 'curl -o /tmp/launches.json -L \'https://ll.thespacedevs.com/2.0.0/launch/upcoming\''
+
+
+with DAG(dag_id='rockets',
+         start_date=airflow.utils.dates.days_ago(14), schedule_interval=None):
+    download = BashOperator(task_id='download',
+                            bash_command=bash_cmd)
+    notify = BashOperator(task_id='notify',
+                          bash_command='echo \'download ok\'')
+    download >> notify
---
+++ init-airflow.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir ./logs ./plugins
+echo -e "AIRFLOW_UID=$(id -u)\nAIRFLOW_GID=0" > .env
+docker-compose up airflow-init
